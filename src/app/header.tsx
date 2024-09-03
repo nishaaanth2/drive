5974ec1bbbfd96a16ea7a649e9efd03102bc88b8
nishaaanth2 <itismenishaanth@gmail.com>
2024-09-03 10:59:56 +0530

    name change

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -14,9 +14,9 @@ export function Header() {
   return (
     <div className="relative z-10 border-b py-4 bg-gray-50">
       <div className="items-center container mx-auto justify-between flex">
-        <Link href="/" className="flex gap-2 items-center text-xl text-black">
-          <Image src="/logo.png" width="50" height="50" alt="file drive logo" />
-          FileDrive
+        <Link href="/" className="flex  items-center text-xl text-black">
+          <Image src="/logo.png" width="30" height="30" alt="file drive logo" className="m-2 mr-0" />
+          <span className="self-end mb-[2.5px] -ml-0.5 text-2xl font-semibold tracking-wide">rive</span>
         </Link>
 
         <SignedIn>
